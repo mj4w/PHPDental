bb8aa6a06f8629e4cff5944bf342b073ea41c93e
mj4w <marcelaribal963@gmail.com>
2024-03-15 09:03:36 +0800

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,11 @@
+<div class="container">   
+   <footer class="footer">
+      <div class="container">
+	  <div class="foot-margin">
+        <p><a>Copyright © 2021 Reverside Medical Hospital. All Rightsreserved at CampCodes</a></p>
+      </div>
+      </div>
+    </footer>
+</div>
+</body>
+</html>
